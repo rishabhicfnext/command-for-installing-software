c590f9bc0dbd73e73fa3ed9678cf982ec9d4d273
rishabhicfnext <rishabh.nigam@icfnext.com>
2022-06-01 14:19:46 +0530

    add wallpaper and git link

---
+++ Test.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Test 
+{
+	public static void main(String ar[])
+	{
+		HashSet<String> set=new HashSet();
+		set.add("Rishabh");
+		set.add("Pandu");    
+           	set.add("Vikas");    
+           	set.add("Vinod");   
+		 
+		System.out.println(set.toString());  		
+	}
+}
